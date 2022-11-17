f10fa9928c52bb2ee6f768cacc99149c01b0b6d7
Elisa Ding <elisa.ding@hotmail.com>
2022-11-17 07:26:25 -0500

    include additional dense layer

--- src/pepms2/modeling.py
+++ src/pepms2/modeling.py
@@ -27,12 +27,20 @@ def cosine_similarity(y_true, y_pred):
 
 def build_model(options, metrics=[cosine_similarity]):
     model = Sequential()
-    model.add(tf.keras.Input(shape=(options.max_sequence_length, options.amino_acid_size())))
+    model.add(Conv1D(
+        filters=64,
+        kernel_size=2,
+        activation="relu",
+        input_shape=(options.max_sequence_length, options.amino_acid_size())
+    ))
+    model.add(Masking(mask_value=0.))
     model.add(Bidirectional(LSTM(128, return_sequences=True)))
     model.add(Dropout(0.5))
     model.add(TimeDistributed(
         Dense(options.intensity_size(), activation='relu')
     ))
+    model.add(Dense(options.intensity_size(), activation='relu'))
+
     model.compile(
         loss="mean_squared_error",
         optimizer="adam",
