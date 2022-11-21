c56238613ca82b04031f13fe02d4eeefb4f05354
Elisa Ding <elisa.ding@hotmail.com>
2022-11-20 21:36:40 -0500

    add dense layer to size 4 kernel

--- src/pepms2/modeling.py
+++ src/pepms2/modeling.py
@@ -38,6 +38,7 @@ def build_model(options, metrics=[cosine_similarity]):
     model.add(TimeDistributed(
         Dense(options.intensity_size(), activation='relu')
     ))
+    model.add(Dense(options.intensity_size(), activation='relu'))
     model.compile(
         loss="mean_squared_error",
         optimizer="adam",
